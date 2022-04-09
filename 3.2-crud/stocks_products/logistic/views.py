0f84dcf31e7b93e94941d8375987f01c29c3fb52
Anton Veldin <anton.veldin@gmail.com>
2022-04-09 18:31:23 +0700

    Final refactoring.

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -9,20 +10,22 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
     filter_backends = [
         DjangoFilterBackend,
         SearchFilter,
     ]
     filterset_fields = ['id']
     search_fields = ['title', 'description']
+    pagination_class.page_size = 2
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    pagination_class = PageNumberPagination
     filter_backends = [
         DjangoFilterBackend,
-        # SearchFilter,
     ]
     filterset_fields = ['products']
-    # при необходимости добавьте параметры фильтрации
+    pagination_class.page_size = 2
