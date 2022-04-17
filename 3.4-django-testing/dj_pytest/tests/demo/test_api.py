36c73a2599cf4588c8677e125084049e764d50fc
Anton Veldin <anton.veldin@gmail.com>
2022-04-17 16:49:05 +0700

    Added in test_api creationa new User and one Message. Added check for length of response and message text

--- 3.4-django-testing/dj_pytest/tests/demo/test_api.py
+++ 3.4-django-testing/dj_pytest/tests/demo/test_api.py
@@ -1,18 +1,28 @@
 import pytest
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
+from demo.models import Message
 
-# Указываем с помощью декоратора, что тест будет работать с бд.
+
 @pytest.mark.django_db
 def test_api():
     # Arrange
-    # Опишем клиента, который будет отправлять запросы на наш api.
     client = APIClient()
+    # Создадим пользователя.
+    User.objects.create_user('admin')
+    # Создадим тестовое сообщение от созданного пользователя.
+    Message.objects.create(user_id=1, text='test_msg')
 
     # Act
-    # Отправим get-запрос на нужный маршрут.
     response = client.get('/messages/')
 
     # Assert
-    # Проверим успешный статус ответа.
     assert response.status_code == 200
+    # Обратимся к содержимому ответа запроса.
+    data = response.json()
+    # Проверим наличие первого тестового сообщения.
+    assert len(data) == 1
+    # Проверим текст ответа созданного сообщения. В ответе словарь.
+    assert data[0]['text'] == 'test_msg'
+
