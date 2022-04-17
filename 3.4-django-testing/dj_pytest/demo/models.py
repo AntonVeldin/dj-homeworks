282942d68e01a76726ef91294c2dadd4a50765b8
Anton Veldin <anton.veldin@gmail.com>
2022-04-17 15:36:15 +0700

    Added Message model.

--- 3.4-django-testing/dj_pytest/demo/models.py
+++ 3.4-django-testing/dj_pytest/demo/models.py
@@ -1,3 +1,13 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Добавим модель Сообщение
+class Message(models.Model):
+# Сообщение оставляет пользователь.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# У сообщения есть текст.
+    text = models.TextField()
+# Время создания сообщения.
+    created_at = models.DateTimeField(auto_now_add=True)
