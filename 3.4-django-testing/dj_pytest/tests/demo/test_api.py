e96e37f0e11aaac6bc43a1e82c9f100bb5c74e6f
Anton Veldin <anton.veldin@gmail.com>
2022-04-17 16:18:11 +0700

    Added test_api. Added rest_framework app to settings.py.

--- 3.4-django-testing/dj_pytest/tests/demo/test_api.py
+++ 3.4-django-testing/dj_pytest/tests/demo/test_api.py
@@ -1,3 +1,18 @@
+import pytest
+from rest_framework.test import APIClient
 
+
+# Указываем с помощью декоратора, что тест будет работать с бд.
+@pytest.mark.django_db
 def test_api():
-    assert True is True
+    # Arrange
+    # Опишем клиента, который будет отправлять запросы на наш api.
+    client = APIClient()
+
+    # Act
+    # Отправим get-запрос на нужный маршрут.
+    response = client.get('/messages/')
+
+    # Assert
+    # Проверим успешный статус ответа.
+    assert response.status_code == 200
