990e9c430b8cbbcd8eb573e9f8d9ceb74586f1a9
Anton Veldin <anton.veldin@gmail.com>
2022-04-17 12:21:59 +0700

    Commet for get-requests exeption in custom permission class IsOwnerOrReadOnly

--- 3.3-permissions/dj_auth_drf_lesson/demo/permission.py
+++ 3.3-permissions/dj_auth_drf_lesson/demo/permission.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
-# Добавим проверку, имеет ли пользователь право на работу с ресурсом.
     def has_object_permission(self, request, view, obj):
+# Добавим исключение при get-запросе.
+# Проверку на собственность будет проходить любой пользователь.
         if request.method == 'GET':
             return True
-# Возвращаем true, если пользователь в запросе совпадает с владельцем из объекта
         return request.user == obj.user
