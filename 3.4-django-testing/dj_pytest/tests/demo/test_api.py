6fa5d7e5c2beb18ea36dffa2b25a8f6dc7b6edf2
Anton Veldin <anton.veldin@gmail.com>
2022-04-18 20:30:42 +0700

    Added client and user fixtures. Added new test - test_create_message.

--- 3.4-django-testing/dj_pytest/tests/demo/test_api.py
+++ 3.4-django-testing/dj_pytest/tests/demo/test_api.py
@@ -5,14 +5,26 @@ from rest_framework.test import APIClient
 from demo.models import Message
 
 
+# Добавим фикстуру, которая будет возвращать клиент.
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+# Добавим фикстуру, которая будет создавать пользователя.
+@pytest.fixture
+def user():
+    return User.objects.create_user('admin')
+
+
 @pytest.mark.django_db
-def test_get_messages():
+def test_get_messages(client, user):
     # Arrange
-    client = APIClient()
+    # client = APIClient()
     # Создадим пользователя.
-    User.objects.create_user('admin')
+    # User.objects.create_user('admin')
     # Создадим тестовое сообщение от созданного пользователя.
-    Message.objects.create(user_id=1, text='test_msg')
+    Message.objects.create(user_id=user.id, text='test_msg')
 
     # Act
     response = client.get('/messages/')
@@ -25,3 +37,19 @@ def test_get_messages():
     assert len(data) == 1
     # Проверим текст ответа созданного сообщения. В ответе словарь.
     assert data[0]['text'] == 'test_msg'
+
+
+@pytest.mark.django_db
+def test_create_message(client, user):
+    # Запросим из бд количество сообщений.
+    count = Message.objects.count()
+
+# Создадим сообщение. Укажем формат передачи данных и в словаре data передадим нужные параметры.
+#     response = client.post('/messages/', data={'user': user.id, 'text': 'test text'}, format='json')
+# Добавим в настройки формат по умолчанию - json.
+    response = client.post('/messages/', data={'user': user.id, 'text': 'test text'}, format='json')
+
+# Проверим сохранение объекта.
+    assert response.status_code == 201
+# Проверим, увеличилось ли количество сообщений после размещения нового.
+    assert Message.objects.count() == count + 1
